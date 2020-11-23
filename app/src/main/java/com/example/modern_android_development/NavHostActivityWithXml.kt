887df6acb67df1d1f438a07a5ac2fac3cd9293a2
hosseinkhojany <hosseinkhojany1380@gmail.com>
2020-11-23 00:47:19 -0500

    dev

--- app/src/main/java/com/example/modern_android_development/NavHostActivityWithXml.kt
+++ app/src/main/java/com/example/modern_android_development/NavHostActivityWithXml.kt
@@ -2,24 +2,33 @@ package com.example.modern_android_development
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.setContent
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.example.modern_android_development.ui.ModernAndroidDevelopmentTheme
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 //Container Single Activity
 class NavHostActivityWithXml : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ModernAndroidDevelopmentTheme{
+        setContentView(R.layout.activity_main)
 
-                val navController = rememberNavController()
-
-
-            }
-        }
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_profile
+            ),
+            drawerLayout
+        )
+        navView.setupWithNavController(navController)
     }
 }
 
